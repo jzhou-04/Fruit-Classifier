c80294720febe5401eb45d072a9d8fc662ced2f8
Jeffrey Zhou <zhoujeffrey@yahoo.com>
2022-08-14 18:03:44 -0500

    Initial Commit

---
+++ Fruit Classifier/Fruit_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruit_ClassifierApp.swift
+//  Fruit Classifier
+//
+//  Created by Jeffrey Zhou on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Fruit_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
